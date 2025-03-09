72c96232662fa4f209666dce37667743e05bb094
Th-am-er <organisedlearning@icloud.com>
2025-03-09 10:19:15 +0000

    Display quote in a string using console.log

---
+++ QuotesInString.js
@@ -0,0 +1,10 @@
+// First method 
+console.log("Yes, I'm ready to learn JS.");
+
+// Second method 
+console.log('Yes, I\'m ready to learn JS.');
+
+// Third method 
+console.log(`Yes, I'm ready to learn JS.`);
+
+
