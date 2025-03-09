264cb7e5b7fbf90b03d598ff975a009607bd8e31
Th-am-er <organisedlearning@icloud.com>
2025-03-09 11:21:44 +0000

    Create a variable and reassign a new value to it

---
+++ Mutable_variables.js
@@ -0,0 +1,4 @@
+let sport = "Tennis"; // Initialize the sport variable with a value
+sport = "Football";   // Update the sport variable to a new value
+
+console.log(sport); // Output the current value of sport
