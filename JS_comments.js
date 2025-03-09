8ec93de68885c073eaea7310f2cead1e40c965be
Th-am-er <organisedlearning@icloud.com>
2025-03-09 10:54:32 +0000

    Show different JS comment types

---
+++ JS_comments.js
@@ -0,0 +1,8 @@
+// Single-line comments
+console.log("Single-line comments"); // This code outputs the message to the console
+
+// Multi-line comments
+console.log("Multi-line comments") /*  
+The following code outputs the message to the console
+The console will display a line with the text "Multi-line comments"
+*/
