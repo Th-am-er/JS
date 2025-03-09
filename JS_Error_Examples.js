ddfe6343a6debbce4a9f2eabc5121eefa11b1539
Th-am-er <organisedlearning@icloud.com>
2025-03-09 10:24:23 +0000

    Show common JavaScript ERRORS

---
+++ JS_Error_Examples.js
@@ -0,0 +1,11 @@
+// First ERROR: Improper function naming!
+consle.log("Hello, World!");
+
+// Second ERROR: Using the wrong letter case in function names! ( JavaScript is case-sensitive)
+CONSOLE.LOG("Hello, World!");
+
+// Third ERROR: Missing one or both quotation marks for a string!
+console.log("JavaScript);
+
+// Four ERROR: Missing parentheses!
+console.log("Hello, World!";
