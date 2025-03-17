0cf166476d8deb74eb25f8b034dc93712bc26e34
Th-am-er <organisedlearning@icloud.com>
2025-03-17 13:16:58 +0000

    Created various anonymous function examples : traditional, incorrect, and arrow function

---
+++ js_anonymous_functions.js
@@ -0,0 +1,14 @@
+// Function assigned to a variable
+const greetings = function() {
+    return "Hello, World!"; // Returns a greeting message
+}
+
+// Anonymous function (incorrect example, should be assigned to a variable)
+// function () {
+//     return "Hello, World!";
+// }
+
+// Arrow function to add two numbers
+const add = (a, b) => {
+    return a + b; // Returns the sum of a and b
+}
